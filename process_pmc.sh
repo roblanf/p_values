2ba65ba51e6edf5c0df85930a8594c99aa812591
roblanf <rob.lanfear@gmail.com>
2024-05-31 17:09:57 +1000

    Update process_pmc.sh

--- process_pmc.sh
+++ process_pmc.sh
@@ -4,15 +4,9 @@
 mkdir -p raw_data/xml_documents
 mkdir -p raw_data/csv_files
 
-# Unzip all .tar.gz files into /raw_data/xml_documents
+# Unzip all .tar.gz files directly into /raw_data/xml_documents
 for tar_file in downloads/*.tar.gz; do
-    tar -xzf "$tar_file" -C raw_data/xml_documents
-done
-
-# Move all the .xml files into the one directory
-for dir in raw_data/xml_documents/*/; do
-    find "$dir" -type f -name "*.xml" -exec mv {} raw_data/xml_documents/ \;
-    rmdir "$dir" # Remove empty directory
+    tar -xzf "$tar_file" -C raw_data/xml_documents --strip-components=1
 done
 
 # Combine all .csv files into one
@@ -22,6 +16,9 @@ head -n 1 $(ls downloads/*.csv | head -n 1) > "$combined_csv" # add header
 for csv_file in downloads/*.csv; do
     tail -n +2 "$csv_file" >> "$combined_csv"
 done
+
+# Don't need this anymore
+rmdir raw_data/csv_files
 
 echo "All XML files are extracted into raw_data/xml_documents"
 echo "Combined CSV file is created at raw_data/combined_filelists.csv"
