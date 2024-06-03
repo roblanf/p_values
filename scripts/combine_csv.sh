6ed9c5b096cfed88ec711fb3fc1799b5e77837f4
roblanf <rob.lanfear@gmail.com>
2024-06-03 15:18:23 +1000

    Update combine_csv.sh

--- scripts/combine_csv.sh
+++ scripts/combine_csv.sh
@@ -1,34 +1,13 @@
 #!/bin/bash
 
-output_file="processed_data/combined_pvalues.csv"
 input_dir="processed_data"
+output_file="processed_data/combined_pvalues.csv"
 
-# Remove the output file if it exists
-rm -f "$output_file"
+# Add header from the first CSV file
+head -n 1 $(find "$input_dir" -type f -name '*.csv' | head -n 1) > "$output_file"
 
-# Find all CSV files in the directory
-csv_files=$(find "$input_dir" -type f -name '*.csv')
+# Add all data excluding headers
+find "$input_dir" -type f -name '*.csv' -exec tail -n +2 {} + >> "$output_file"
 
-# Check if there are any CSV files to process
-if [ -z "$csv_files" ]; then
-  echo "No CSV files found in $input_dir"
-  exit 1
-fi
-
-# Add the header from the first CSV file
-first_file=$(echo "$csv_files" | head -n 1)
-head -n 1 "$first_file" > "$output_file"
-
-# Loop through all CSV files and append their content to the output file (excluding headers)
-for csv_file in $csv_files; do
-
-  echo $csv_file
-
-  # Skip the header for all but the first file
-  tail -n +2 "$csv_file" >> "$output_file"
-  # Remove the processed CSV file
-  rm -f "$csv_file"
-
-done
-
-echo "Combined CSV file created at $output_file"
+# Clean up individual CSV files
+find "$input_dir" -type f -name 'PMC*.csv' -exec rm {} \;
