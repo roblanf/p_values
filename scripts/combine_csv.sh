61b9c0caf743dec77c4928eee58d5bef9712f6a5
roblanf <rob.lanfear@gmail.com>
2024-06-03 15:51:23 +1000

    use rm
    to clean up quicker

--- scripts/combine_csv.sh
+++ scripts/combine_csv.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 input_dir="processed_data"
-output_file="processed_data/combined_pvalues.csv"
+output_dir="combined_output"
+output_file="$output_dir/combined_pvalues.csv"
+
+# Create the output directory if it doesn't exist
+mkdir -p "$output_dir"
 
 # Add header from the first CSV file
 head -n 1 $(find "$input_dir" -type f -name '*.csv' | head -n 1) > "$output_file"
 
 # Add all data excluding headers
-find "$input_dir" -type f -name '*.csv' -exec tail -n +2 {} + >> "$output_file"
+find "$input_dir" -type f -name 'PMC*.csv' -exec tail -n +2 {} + >> "$output_file"
 
-# Clean up individual CSV files
-find "$input_dir" -type f -name 'PMC*.csv' -exec rm {} \;
+# Move the output file back to the original directory if needed
+# mv "$output_file" "$input_dir/combined_pvalues.csv"
+
+# Clean up the individual CSV files by removing the entire directory
+rm -rf "$input_dir"
+
+echo "Combined CSV file created at $output_file"
+echo "All individual CSV files removed."
