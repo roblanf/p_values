2d245aec8117773d3cd7b6f2c2357338475060ae
roblanf <rob.lanfear@gmail.com>
2024-06-03 14:22:23 +1000

    Update combine_csv.sh

--- scripts/combine_csv.sh
+++ scripts/combine_csv.sh
@@ -6,16 +6,29 @@ input_dir="processed_data"
 # Remove the output file if it exists
 rm -f "$output_file"
 
+# Find all CSV files in the directory
+csv_files=$(find "$input_dir" -type f -name '*.csv')
+
+# Check if there are any CSV files to process
+if [ -z "$csv_files" ]; then
+  echo "No CSV files found in $input_dir"
+  exit 1
+fi
+
 # Add the header from the first CSV file
-first_file=$(find "$input_dir" -type f -name '*.csv' | head -n 1)
+first_file=$(echo "$csv_files" | head -n 1)
 head -n 1 "$first_file" > "$output_file"
 
 # Loop through all CSV files and append their content to the output file (excluding headers)
-for csv_file in "$input_dir"/*.csv; do
+for csv_file in $csv_files; do
+
+  echo $csv_file
+
   # Skip the header for all but the first file
   tail -n +2 "$csv_file" >> "$output_file"
   # Remove the processed CSV file
   rm -f "$csv_file"
+
 done
 
 echo "Combined CSV file created at $output_file"
